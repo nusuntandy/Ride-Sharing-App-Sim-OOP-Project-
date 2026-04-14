aa4e624a35b17f0187d0df746afd2c4d676c5096
Nedelcu Andy <constantin.andy80@gmail.com>
2026-04-14 17:14:52 +0300

    Update Ride.cpp

--- src/Ride.cpp
+++ src/Ride.cpp
@@ -13,16 +13,36 @@ Ride::Ride(Client* c, Driver* d, std::string lS, std::string lF) {
     locatieFinal = lF;
 
     currentStatus = REQUESTED;
+
+    std::cout << "[Constructor] Class Ride called\n";
+}
+
+Ride::~Ride() {
+    nextID--;
+    driver->setDisponibil(true);
+    std::cout << "[Destructor] Class Ride called\n";
+}
+
+Client* Ride::getClient(void ) {
+    return this->client;
+}
+
+Driver* Ride::getDriver(void ) {
+    return this->driver;
 }
 
 void Ride::updateStatus(enum status stat) {
     currentStatus = stat;
 
-    if(currentStatus == ACCEPTED)
-        driver->acceptRide();
-    else if(currentStatus == COMPLETED)
+    if(currentStatus == COMPLETED) {
+        if(client->getBalance() < calculeazaPret())
+            throw std::runtime_error("Payment error: Balanta insuficienta pentru a completa tranzactia.");
+    
+        client->setBalance(client->getBalance() - calculeazaPret());
+        driver->setBalance(driver->getBalance() + calculeazaPret());
         driver->finishRide();
-    else if(currentStatus == CANCELED && !driver->getDisponibil())
+    }
+    if(currentStatus == CANCELED && !driver->getDisponibil())
         driver->finishRide();
 }
 
@@ -36,19 +56,28 @@ void Ride::showStatus() const {
 
     std::cout << "Status: ";
     switch(currentStatus) {
-        case REQUESTED: std::cout << "REQUESTED"; break;
+        case REQUESTED: 
+            std::cout << "REQUESTED"; 
+            break;
 
-        case ACCEPTED: std::cout << "ACCEPTED"; break;
+        case ACCEPTED: 
+            std::cout << "ACCEPTED"; 
+            break;
 
-        case ONGOING: std::cout << "ONGOING"; break;
+        case ONGOING: 
+            std::cout << "ONGOING"; 
+            break;
 
-        case COMPLETED: std::cout << "COMPLETED"; break;
+        case COMPLETED: 
+            std::cout << "COMPLETED"; 
+            break;
 
-        case CANCELED: std::cout << "CANCELED"; break;
+        case CANCELED: 
+            std::cout << "CANCELED"; 
+            break;
     }
 
     std::cout << "\n";
-
     if(currentStatus == COMPLETED) 
         std::cout << "Pret Final: " << this->calculeazaPret() << " LEI\n";
 }
@@ -57,9 +86,44 @@ int Ride::calculeazaPret() const {
     return 10 * (locatieStart.length() + locatieFinal.length());
 }
 
-Ride* requestRide(Client* client, Driver* drivers, const std::string lS, const std::string lF) {
+Ride* requestRide(Client* client, Driver* drivers, int dL, const std::string lS, const std::string lF) {
     std::cout << client->getNume() << " a solicitat o cursa\n";
     std::cout << "Se cauta soferi..." << "\n";
 
-    return new Ride(client, drivers, lS, lF);
+    for(int i = 0; i < dL; i++) {
+        int dRand = rand() % dL;
+
+        if(drivers[dRand].getDisponibil())
+            return new Ride(client, &drivers[dRand], lS, lF);
+    }
+
+    throw std::runtime_error("Nu au fost gasiti soferi disponibili!");
+    return nullptr;
+}
+
+void cancelRide(const Client *c, Ride **ride) {
+    std::cout << c->getNume() << " a anulat cursa.\n";
+
+    Ride *currentRide = *ride;
+    currentRide->updateStatus(CANCELED);
+    delete currentRide;
+
+    *ride = nullptr;
+}
+
+void cancelRide(const Driver *d, Ride **ride) {
+    std::cout << d->getNume() << " a anulat cursa.\n";
+
+    Ride *currentRide = *ride;
+    currentRide->updateStatus(CANCELED);
+    delete currentRide;
+
+    *ride = nullptr;
+}
+
+void acceptRide(Driver *d, Ride *ride) {
+    std::cout << d->getNume() << " a acceptat cursa.\n";
+    d->setDisponibil(false);
+
+    ride->updateStatus(ACCEPTED);
 }
