8c8a7078c6e7c68955af95d474c14e1984bb4a03
Nedelcu Andy <constantin.andy80@gmail.com>
2026-04-14 17:15:37 +0300

    Update Client.h

--- includes/Client.h
+++ includes/Client.h
@@ -1,15 +1,11 @@
 #ifndef Client_h
 #define Client_h
-class Ride;
-
 class Client : public User {
 public:
-    Client(std::string nume, float rating);
+    Client(std::string nume, int age, float rating, int balance);
 
-    void cancelRide(Ride **ride);
+    ~Client();
 
-    void afisare() const override;
-
-    friend Ride *requestRide(Driver* d, std::string lS, std::string lF);
+    void afisare() const override;    
 };
 #endif
