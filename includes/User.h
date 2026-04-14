40fac8b9094b54c20579591f3b933a7080382001
Nedelcu Andy <constantin.andy80@gmail.com>
2026-04-14 17:16:31 +0300

    Update User.h

--- includes/User.h
+++ includes/User.h
@@ -4,23 +4,30 @@ class User {
 protected:
     int id;
     std::string nume;
+    int age;
+    int balance;
     float rating;
 
     static int nextID;
     int static genereazaID();
 
 public:
-    User();
-    User(std::string nume, float rating);
+    User(std::string nume, int age, float rating, int balance);
+
+    ~User();
 
     int getID() const;
     std::string getNume() const;
+    int getAge() const;
+    int getBalance() const;
     float getRating() const;
 
     void setID(int ID);
     void setNume(std::string nume);
+    void setAge(int a);
+    void setBalance(int b);
     void setRating(float rating);
 
-    virtual void afisare() const;
+    virtual void afisare() const = 0;
 };
 #endif
